7ff08805ebcaebe951c4462a1db1b1b5f7930fda
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-06 20:34:36 +0000

    first runthrough of project

---
+++ React Simple Counter/src/App.jsx
@@ -0,0 +1,42 @@
+
+import { useState, useEffect } from 'react'
+import SimpleCounter from "./SimpleCounter"
+
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+
+    // component mounting
+
+    const interval = setInterval(() => {
+      setCounter(counter => counter + 1)
+    }, 1000)
+
+    // component unmount
+
+    return () => clearInterval(interval)
+  }, [counter])
+
+  // if there is no dependancy array, it will just change as needed
+  // an empty dependancy array, will just run once 
+  // array with a state variable: will only update when the state changes like with the setInterval declared which made it run every second
+
+  function calculateSeconds(aCounter, placeValue) {
+    return Math.floor(aCounter / placeValue) % 10
+  }
+
+  return (
+    <>
+      <SimpleCounter
+        thousandsDigit = {calculateSeconds(counter, 1000)}
+        hundredsDigit = {calculateSeconds(counter, 100)}
+        tensDigit = {calculateSeconds(counter, 10)}
+        onesDigit = {calculateSeconds(counter, 1)}
+      />
+    </>
+  )
+}
+
+export default App
